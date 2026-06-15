860a986e03a21c5691097d37db85c842d7d26cfe
Yashin Mithani <yashinmithani950@gmail.com>
2026-06-15 10:33:35 +0530

    Update Game.js

--- 12 jun/Game.js
+++ 12 jun/Game.js
@@ -1,27 +1,32 @@
-for (;;) {
-    alert("Welcome To Our Game Programme...")
-    let randomNumber = Number(prompt("Enter The Number (1 To 100)"));
+let playAgain = true;
 
-    if (randomNumber < 1 || randomNumber > 100) {
-        alert("Invalid Number...!");
-        continue;
+for (;;) {
+
+    if (!playAgain) {
+        break;
     }
+
+    alert("Welcome To Our Game Programme...");
+
+    let random = parseInt(Math.random() * 100) + 1;
 
     let Count = 0;
     let maxCount = 10;
+    let isWin = false;
 
     alert("Guess The Number");
     alert("You Have Only 10 Attempts.");
 
     for (;;) {
-        if (Count >= maxCount) {
+
+        if (isWin || Count >= maxCount) {
             alert("Game Over!");
-            alert("Correct Number Was : " + randomNumber);
+            alert("Correct Number Was : " + random);
             break;
         }
 
-        let guess = Number(prompt("Enter Your Guess :"));
-        
+        let guess = Number(prompt("Enter Your Guess (1-100) :"));
+
         if (guess < 1 || guess > 100) {
             alert("Enter Number Between 1 To 100");
             continue;
@@ -29,28 +34,28 @@ for (;;) {
 
         Count++;
 
-        if (guess > randomNumber) {
+        if (guess > random) {
             alert("Too High");
             alert("Attempts Left : " + (maxCount - Count));
         }
-        else if (guess < randomNumber) {
+        else if (guess < random) {
             alert("Too Low");
             alert("Attempts Left : " + (maxCount - Count));
         }
         else {
+            isWin = true;
             alert("Congratulations! You Win");
             alert("Total Attempts : " + Count);
-            break;
         }
     }
 
-    let playAgain = prompt("Play Again ? (yes / no)");
+    playAgain = prompt("Play Again ? (yes / no)");
 
-    if (playAgain == "yes") {
-        continue;
+    if (playAgain.toLowerCase() == "yes") {
+        playAgain = true;
     }
     else {
+        playAgain = false;
         alert("Thank You For Playing!");
-        break;
     }
 }
