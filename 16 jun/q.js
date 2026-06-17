76cc682974bc2ff7b9b8756c8a5d6eb91a6abcbd
yashin <yashinmithani950@gmail.com>
2026-06-17 08:34:49 +0530

    All

---
+++ 16 jun/q.js
@@ -0,0 +1,55 @@
+// Q1. Write a named function to print "Hello World".
+
+// function print()
+// {
+//     console.log("Hello World")
+// }
+
+// print();
+
+// Q4. Write a named function that prints the multiplication table of a given number.
+
+// function multiplication(n1)
+// {
+//     for (let i = 0; i <= 10; i++) {
+
+//         console.log(n1 + " X " + i + " = " + (n1 * i));
+//     }
+// }
+
+// multiplication(4);
+
+// Q6. Write a named function that returns a random number between 1 and 100.
+
+// function rnd()
+// {
+//     return parseInt(Math.random() * 100) + 1;
+// }
+
+// console.log(rnd());
+
+// Q7. Write a named function that returns the sum of two numbers.
+
+
+// function sum(n1,n2)
+// {
+//     return n1 + n2;
+// }
+
+// console.log(sum(12,16));
+
+// Write a named function that returns the factorial of a number.
+
+// function factorial(n1)
+// {
+//     let fact = 1;
+
+//     for(let i = 1; i <= n1;i++)
+//     {
+//         fact *= i;
+//     }
+
+//     return fact;
+// }
+
+// console.log(factorial(5));
