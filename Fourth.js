658bb4c00ca1e9cb6e87f9733c40b0a224ac02e4
yashin <yashinmithani950@gmail.com>
2026-06-24 09:25:07 +0530

    all

--- Fourth.js
+++ Fourth.js
@@ -1,9 +1,7 @@
-console.log("Hello Guys")
-
-let letters = ['b' , 'c'];
+let letters = ['b', 'c'];
 
 letters.unshift('a');
-console.log("After Unshift : ",letters);
+console.log("After Unshift : ", letters);
 
 letters.shift();
 console.log("After Shift : ", letters)
